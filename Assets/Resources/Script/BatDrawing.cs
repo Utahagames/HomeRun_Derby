217bf6ee5a92dc635543d2d444284538f471996a
Kasumioka <al19074@shibaura-it.ac.jp>
2023-08-02 13:47:10 +0900

    バット描画・上下移動実装

---
+++ Assets/Resources/Script/BatDrawing.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatDrawing : MonoBehaviour
+{
+    [SerializeField] GameObject Bat_HandSide;
+    [SerializeField] GameObject Bat_EdgeSide;
+
+    [SerializeField] LineRenderer _lineRenderer;
+
+    [SerializeField] EdgeCollider2D _edgeCollider2D;
+
+    Transform transform_HandSide;
+    Transform transform_EdgeSide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform_HandSide = Bat_HandSide.transform;
+        transform_EdgeSide = Bat_EdgeSide.transform;
+
+        DrawingBat();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DrawingBat()
+    {
+        Vector3[] positions = new Vector3[]
+        {
+            transform_HandSide.position,               // 開始点
+            transform_EdgeSide.position               // 終了点
+        };
+
+        //始点、終点の幅を変更
+        _lineRenderer.startWidth = 0.5f;
+        _lineRenderer.endWidth = 0.5f;
+
+        // 線を引く場所を指定する
+        _lineRenderer.SetPositions(positions);
+
+        SettingCollider();
+    }
+
+    void SettingCollider()
+    {
+        List<Vector2> collider_positions = new List<Vector2>
+        {
+            new Vector2( transform_HandSide.position.x ,transform_HandSide.position.y),               // 開始点
+            new Vector2( transform_EdgeSide.position.x ,transform_EdgeSide.position.y)               // 終了点
+        };
+
+        _edgeCollider2D.SetPoints(collider_positions);
+    }
+}
---
+++ Assets/Resources/Script/BatMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatMovement : MonoBehaviour
+{
+    [SerializeField] GameObject Bat_HandSide;
+    [SerializeField] GameObject Bat_EdgeSide;
+
+    [SerializeField] Vector3 moveUP_Speed;
+    [SerializeField] Vector3 moveDown_Speed;
+
+    [SerializeField] BatDrawing batDrawing;
+
+    Transform transform_HandSide;
+    Transform transform_EdgeSide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform_HandSide = Bat_HandSide.transform;
+        transform_EdgeSide = Bat_EdgeSide.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(transform_HandSide.position.y <= 1.5f)
+            {
+                print("W");
+                transform_HandSide.position += moveUP_Speed * Time.deltaTime;
+
+                batDrawing.DrawingBat();
+            } 
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (transform_HandSide.position.y >= -1.5f)
+            {
+                print("S");
+                transform_HandSide.position += moveDown_Speed * Time.deltaTime;
+
+                batDrawing.DrawingBat();
+            }
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (transform_EdgeSide.position.y <= 1.5f)
+            {
+                print("UP");
+                transform_EdgeSide.position += moveUP_Speed * Time.deltaTime;
+
+                batDrawing.DrawingBat();
+            }
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (transform_EdgeSide.position.y >= -1.5f)
+            {
+                print("Down");
+                transform_EdgeSide.position += moveDown_Speed * Time.deltaTime;
+
+                batDrawing.DrawingBat();
+            }
+        }
+    }
+}
